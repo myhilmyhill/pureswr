4f030d5a39f0eee9dddf0aacf628bf46be01ceed
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2025-09-25 22:19:38 +0900

    refactor

--- app/src/main/java/myhilmyhill/pureswr/ui/SettingsDialog.kt
+++ app/src/main/java/myhilmyhill/pureswr/ui/SettingsDialog.kt
@@ -1,9 +1,6 @@
 package myhilmyhill.pureswr.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -13,11 +10,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import myhilmyhill.pureswr.data.model.Credentials // Make sure this path is correct
+import myhilmyhill.pureswr.data.model.Credentials
 import myhilmyhill.pureswr.ui.theme.PureswrTheme
 
 @Composable
@@ -61,7 +56,7 @@ fun SettingsDialog(
         confirmButton = {
             TextButton(
                 onClick = {
-                    val newCredentials = Credentials(baseUrl.trim(), username.trim(), password) // password usually not trimmed
+                    val newCredentials = Credentials(baseUrl.trim(), username.trim(), password)
                     onSave(newCredentials)
                 }
             ) {
